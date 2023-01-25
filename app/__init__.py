4533b02fc80e654e374e290cec6eb3a801651e53
Oleg Remizov <job.rem1zoff@gmail.com>
2023-01-25 16:32:28 +0300

    add flask-admin

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 
 from app.config import BaseConfig
+
 
 
 def create_app():
@@ -15,6 +18,9 @@ app = create_app()
 db = SQLAlchemy(app)
 db.init_app(app)
 
+from app.models import Skill
+admin = Admin(app, name='Resume', template_mode='bootstrap4')
+admin.add_view(ModelView(Skill, db.session, name="Навыки"))
 
 from app.views import *
 
