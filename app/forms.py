16f9767f1b39422c0a786e2f7436b1b45b0009cf
Oleg Remizov <job.rem1zoff@gmail.com>
2023-02-08 16:13:40 +0300

    post create completed

--- app/forms.py
+++ app/forms.py
@@ -2,5 +2,5 @@ from wtforms import Form, StringField, TextAreaField
 
 
 class PostForm(Form):
-    title = StringField()
-    body = TextAreaField()
+    title = StringField('Title')
+    body = TextAreaField('Body')
--- app/views.py
+++ app/views.py
@@ -1,11 +1,11 @@
-from flask import render_template, request
+from flask import render_template, request, url_for, redirect
 from app.models import Skill, Post
 from app.forms import PostForm
 from flask_admin import  AdminIndexView, expose
 from flask_security import login_required
 
 
-from app import app
+from app import app, db
 
 
 
@@ -16,8 +16,19 @@ class DashboardView(AdminIndexView):
 	   return self.render('admin/dashboard.html')
 
 
-@app.route('/blog/create')
+@app.route('/blog/create', methods=['GET', 'POST'])
 def create_post():
+	if request.method == 'POST':
+		title = request.form['title'] # form также как и args - словарь, title = название поля из форм
+		body = request.form['body']
+		try:
+			post = Post(title=title, body=body)
+			db.session.add(post)
+			db.session.commit()
+		except:
+			print("Something wrong!")
+		return redirect(url_for('blog'))
+		
 	form = PostForm()
 	return render_template('blog/create_post.html', form=form)
 
